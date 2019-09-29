9fd926e14a2b65724f7597bb22a2e2df659e6602
Kamil <mihaandkam@gmail.com>
2019-09-30 00:43:09 +0300

    Многоч фич + рефакторинг

--- ezldap.sh
+++ ezldap.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-#Get file name---------------
-_self="${0##*/}"
-#----------------------------
-
-#Create config----------------
+#########################CONFIG############################
+#Create Config---------------
 function CONFIG {
-cat > /etc/ezldap.conf <<EOF
+echo "Can't find your config file.."
+echo ""
+echo "Starting to create a new one"
+echo "Config file will be located in /etc/ezldap/"
+sudo mkdir /etc/ezldap
+sudo bash -c 'cat > /etc/ezldap/ezldap.conf <<EOF
 ####################################
 #    Config file for Easy-LDAP     #
 ####################################
@@ -28,40 +30,95 @@ password="your_pass"
 search_base="dc=super,dc=domain,dc=com"
 
 ####################################
-EOF
+EOF'
 
-"${EDITOR:-nano}" /etc/ezldap.conf
+sudo "${EDITOR:-nano}" /etc/ezldap/ezldap.conf
 
 exit 1
 }
 #----------------------------
 
-#Check config-----------------
-FILE=/etc/ezldap.conf
+#Check config----------------
+FILE=/etc/ezldap/ezldap.conf
 if test -f "$FILE";
 then
-    source /etc/ezldap.conf
+    source /etc/ezldap/ezldap.conf
 else
-    if [ "$EUID" -ne 0 ]
-    then echo "Please run 'sudo $_self'"
-        exit 1
-    else
-        CONFIG
-    fi
-fi
-#-----------------------------
-
-#Get user name---------------
-if ! [ $(id -u) = 0 ];
-then
-    current_user=$USER
-else
-    current_user=$SUDO_USER
+    CONFIG
 fi
 #----------------------------
+###########################END#############################
+
+
+###########################LDAP############################
+LDAP_TYPE="search"
+
+#LDAP query------------------
+function LDAP_QUERY {
+    if [ $LDAP_TYPE == "search" ]
+    then
+        #Filter options---------------
+        if [ -z "$GROUP" ]
+        then
+            filter="(&(objectCategory=Person)(cn=*$NAME)(sAMAccountName=*$LOGIN))"
+            basic_attribute="name sAMAccountName mail physicalDeliveryOfficeName telephoneNumber birthDate"
+        else
+            filter="(&(objectClass=group)(cn=*$GROUP))"
+            basic_attribute="member distinguishedName name sAMAccountName managedBy mail mailNickname description"
+        fi
+        #-----------------------------
+
+        ldapsearch \
+        -H ldaps://$domain \
+        -D "$username" \
+        -w "$password" \
+        -b "$search_base" \
+        $filter \
+        $basic_attribute \
+        $ATTRIBUTE \
+        | egrep -v "^# .+" | egrep -v "^ref: .+"
+    else
+echo "dn: $group_dn
+changetype: modify
+$modify_type: member
+member: $user_dn" | \
+        ldapmodify \
+        -H ldaps://$domain \
+        -D "$username" \
+        -w "$password" \
+        | egrep -v "^# .+" | egrep -v "^ref: .+"
+    fi
+}
+#----------------------------
+
+#Modify----------------------
+function MODIFY {
+    read -p "'add' or 'delete' user from group: " modify_type
+    read -p "Copy 'DN' group attribute and paste here: " group_dn
+    read -p "Copy 'DN' user attribute and paste here: " user_dn
+    echo ""
+    echo ""
+    LDAP_TYPE="modify"
+    LDAP_QUERY
+}
+#----------------------------
+###########################END#############################
 
 #Help------------------------
 function HELP {
+    #Get file name----------------
+    _self="${0##*/}"
+    #-----------------------------
+    
+    #Get user name----------------
+    if ! [ $(id -u) = 0 ];
+    then
+        current_user=$USER
+    else
+        current_user=$SUDO_USER
+    fi
+    #-----------------------------
+
     echo "Options:"
     echo ""
     echo "-h                        show help"
@@ -84,12 +141,13 @@ function HELP {
 #----------------------------
 
 #Flag options----------------
-while getopts :n:l:g:a:h flag; do
+while getopts :n:l:g:a:mh flag; do
     case $flag in
         n) NAME=${OPTARG} ;;
         l) LOGIN=${OPTARG} ;;
         g) GROUP=${OPTARG} ;;
         a) ATTRIBUTE=${OPTARG} ;;
+        m) MODIFY ;;
         h) HELP ;;
         \?) printf "illegal option: -%s\n" "$OPTARG" >&2
             HELP
@@ -100,23 +158,6 @@ done
 shift $((OPTIND-1))
 #----------------------------
 
-#Filter options--------------
-if [ -z "$GROUP" ]
-then
-    filter="(&(objectCategory=Person)(cn=*$NAME)(sAMAccountName=*$LOGIN))"
-    basic_attribute="name sAMAccountName mail physicalDeliveryOfficeName telephoneNumber birthDate"
-else
-    filter="(&(objectClass=group)(cn=*$GROUP))"
-    basic_attribute="member distinguishedName name sAMAccountName managedBy mail mailNickname description"
-fi
+#Execute query---------------
+LDAP_QUERY
 #----------------------------
-
-ldapsearch \
--H ldaps://$domain \
--D "$username" \
--w "$password" \
--b "$search_base" \
-$filter \
-$basic_attribute \
-$ATTRIBUTE \
-| egrep -v "^# .+" | egrep -v "^ref: .+"

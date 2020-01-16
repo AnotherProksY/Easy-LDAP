413b0221fadfbb138eaa12e475172d15c377bd39
AnotherProksY <mihaandkam@gmail.com>
2020-01-16 15:09:20 +0300

    1. Добавил новую папку assets/ 2. Заменил cat >> EOF на cp assets/<file> /some/path 3. Косметические изменения в ezldap и install.sh

--- install.sh
+++ install.sh
@@ -2,38 +2,32 @@
 
 #########################INSTALL###########################
 if [ ! -d "$HOME/bin" ]; then
-    echo "Creating ~/bin"
-    mkdir ~/bin
-    sleep 1
-    echo ""
+  echo "Creating ~/bin"
+  mkdir $HOME/bin
+  sleep 1
+  echo ""
 fi
 
 FILE=~/.bash_profile
-if test -f "$FILE";
-then
-    echo 'Adding ~/bin to $PATH .bash_profile'
-    cat >> ~/.bash_profile <<EOF
-export PATH=~/bin:$PATH
-EOF
-    source ~/.bash_profile
-    sleep 1
-    echo ""
+if test -f "$FILE"; then
+  echo 'Adding ~/bin to $PATH .bash_profile'
+  cat assets/bash.md >> ~/.bash_profile
+  source ~/.bash_profile
+  sleep 1
+  echo ""
 else
-    echo "Creating ~/.bash_profile .."
-    touch ~/.bash_profile
-    echo 'Adding ~/bin to $PATH .bash_profile'
-    cat >> ~/.bash_profile <<EOF
-export PATH=~/bin:$PATH
-EOF
-    source ~/.bash_profile
-    sleep 1
-    echo ""
+  echo "Creating ~/.bash_profile .."
+  touch ~/.bash_profile
+  echo 'Adding ~/bin to $PATH .bash_profile'
+  cat assets/bash.md >> ~/.bash_profile
+  source ~/.bash_profile
+  sleep 1
+  echo ""
 fi
 
 echo "Do you want to copy file or create symlink ?" 
 read -p "'1 copy' || '2 symlink' [1/2]: " answer
-if [ $answer == "1" ]
-then
+if [ $answer == "1" ]; then
   echo "Copying ezldap to ~/bin/"
   cp ezldap ~/bin/;sudo chmod +x ~/bin/ezldap
 else
@@ -46,47 +40,22 @@ echo ""
 
 
 #########################CONFIG############################
-if ! [ $(id -u) = 0 ];
-then
-    current_user=$USER
+if ! [ $(id -u) = 0 ]; then
+  current_user=$USER
 else
-    current_user=$SUDO_USER
+  current_user=$SUDO_USER
 fi
 
 function CONFIG {
-echo "Starting to create your config file.."
-echo "Config file will be located in /etc/ezldap/"
-sleep 2
-sudo mkdir /etc/ezldap
-sudo bash -c 'cat > /etc/ezldap/ezldap.conf <<EOF
-####################################
-#    Config file for Easy-LDAP     #
-####################################
-# Write your credentials correctly #
-#  All info must be specified in   #
-#          double quotes           #
-####################################
-
-# Your Domain for ldaps://..
-domain="super.domain.com"
-
-# Your username for auth
-username="user_name@super.domain.com"
-
-# Your pass for auth
-password="auth_password"
-
-# Base DC
-search_base="dc=super,dc=domain,dc=com"
-
-####################################
-EOF'
-
-sudo "${EDITOR:-nano}" /etc/ezldap/ezldap.conf
-sudo chmod go-r /etc/ezldap/ezldap.conf
-sudo chown $current_user /etc/ezldap/ezldap.conf
-
-exit 1
+  echo "Starting to create your config file.."
+  echo "Config file will be located in /etc/ezldap/"
+  sleep 2
+  sudo mkdir /etc/ezldap
+  sudo cp assets/ezldap.conf /etc/ezldap/
+  sudo "${EDITOR:-nano}" /etc/ezldap/ezldap.conf
+  sudo chmod go-r /etc/ezldap/ezldap.conf
+  sudo chown $current_user /etc/ezldap/ezldap.conf
+  exit 1
 }
 
 CONFIG
